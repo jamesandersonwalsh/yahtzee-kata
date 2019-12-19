b3f792130e3f7cc43c492826d1c14bf229f00026
James Walsh <james-walsh@pluralsight.com>
2019-12-19 10:55:37 -0700

    Add test harness & example tests

---
+++ src/test/index.test.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect
+
+const subject = require('../index')
+
+describe('test examples to delete', () => {
+  it('should be able to pass', () => {
+    expect(true).to.be.true
+  })
+
+  it('should be able to fail', () => {
+    expect(true).to.be.false
+  })
+})
